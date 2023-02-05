3955eac7a6f7ddc704a5996d62b1391d20afd76a
Oleksii <lesha.avdeev@gmail.com>
2023-02-05 15:38:53 +0200

    Create square divs

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector("#container");
+const flexLine = document.createElement('div');
+flexLine.classList.add('line');
+const divSquare = document.createElement('div');
+divSquare.classList.add('square')
+
+for (let i = 0; i < 16; i++){
+    let newLine = flexLine.cloneNode(true) 
+    for (let j = 0; j < 16; j++){
+        let newSquare = divSquare.cloneNode(true);
+        newLine.appendChild(newSquare);
+    }
+    container.appendChild(newLine);
+}
